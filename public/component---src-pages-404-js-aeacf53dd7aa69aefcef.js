4532f99b668aa88a59fae156616e53883238fc30
mhousetree <mhousetree@gmail.com>
2021-04-14 14:06:55 +0900

    :tada: Initial commit

--- public/component---src-pages-404-js-aeacf53dd7aa69aefcef.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var l=n(7294),u=n(309),a=n(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(a.Z,{title:"404: Not found"}),l.createElement("section",null,l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-aeacf53dd7aa69aefcef.js.map
